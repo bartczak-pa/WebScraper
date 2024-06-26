427a5a2f072e969a0ff0854dfa27e6d99cfa5c03
Pawel Bartczak <pawel@bartczak.tech>
2024-06-26 21:02:29 +0100

    [Refactor] Modified file processing functions.

--- utilities/file_processing.py
+++ utilities/file_processing.py
@@ -1,16 +1,18 @@
 import json
+from pathlib import Path
 
 
 # Function dumping scraped recipes to JSON file for further processing
-def save_data_to_json(data: dict, filename: str):
-    with open(filename, "w") as file:
+def save_data_to_json(data: dict, filename: str) -> None:
+    """Save data to json file."""
+    with Path(filename).open("w") as file:
         json.dump(data, file)
 
 
 # Helper function loading json file as recipes dictionary
-def load_data(filename: str) -> dict:
-    # TODO possibility of selecting file to load
+def load_data(filename: Path) -> dict:
+    """Load data from json file."""
+    # TODO @Pawel: possibility of selecting file to load  # noqa: FIX002, TD003
 
-    file = open(filename)
-    data = json.load(file)
-    return data
+    with Path.open(filename) as data:
+        return json.load(data)
