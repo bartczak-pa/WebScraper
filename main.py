e016ede6550391ba0924a2879bee5e870acc549a
Pawel Bartczak <pawel@bartczak.tech>
2024-07-07 21:12:37 +0100

    Added method checking new recipes.
    check_new_recipes_from_category() compares dictionary of existing recipes and parse any new recipes from given category.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     """Initialisation of main and updates scrapers."""
     scraper = Scraper(categories={}, recipes={})
     updates_scraper = UpdatesScraper()
+    updates_scraper.check_new_category_recipes("Breakfast", "https://biancazapatka.com/en/recipes/breakfast/")
 
 
 
--- scraper/updates_scraper.py
+++ scraper/updates_scraper.py
@@ -13,22 +13,38 @@ class UpdatesScraper(Scraper):
 
     def __init__(self) -> None:
         super().__init__({}, {})
+        self.existing_categories: dict = load_data(Path("../json_files/categories.json"))
+        self.existing_recipes: dict = load_data(Path("../json_files/parsed_recipes.json"))
 
     def check_new_categories(self) -> str:
-        """Check for new categories."""
-        existing_categories: dict = load_data(Path("../json_files/categories.json"))
+        """Check for new categories and compare them with dictionary of existing categories."""
         new_categories: dict = self.parse_category_urls()
         message: str = ""
 
-        if len(existing_categories) == len(new_categories):
+        if len(self.existing_categories) == len(new_categories):
             message = "No new categories have been found."
-        elif len(new_categories) > len(existing_categories):
+        elif len(new_categories) > len(self.existing_categories):
             message = "New categories have been found."
             save_data_to_json(new_categories, "../json_files/categories.json")
         return message
 
-    def scrape_new_recipes(self) -> None:
-        """Scrape new recipes from all categories."""
-        self.recipes = self.parse_category_urls()
-        self.delete_recipes_without_ingredients(self.recipes)
-        self.save_data_to_json(self.recipes, "../json_files/recipes_urls.json")
+    def check_new_recipes_from_category(self, category_name: str, category_url: str) -> None:
+        """Check for new recipes in a given category and saves them to the dictionary of existing recipes."""
+        message: str = ""
+        new_recipes: dict = self.parse_recipes_urls(category_name, category_url, 1)
+
+        for recipe, values in new_recipes.items():
+            if recipe not in self.existing_recipes:
+                message = f"New recipes have been found in {category_url}."
+                url: str = values["url"]
+
+                self.existing_recipes[recipe]: dict = {
+                    "category": category_name,
+                    "url": url,
+                    "content": self.parse_recipe_details(url),
+                }
+                save_data_to_json(self.existing_recipes, "../json_files/parsed_recipes.json")
+            else:
+                message = "No new recipes have been found."
+        # TODO (Pawel): Replace printing with logging
+        print(message)  # noqa: T201
