bd806b942689e6358a92c9a83dd4587b45c917bc
Pawel Bartczak <pawel@bartczak.tech>
2024-07-09 19:18:41 +0100

    Added function displaying amount of recipes from each category.

--- api/api.py
+++ api/api.py
@@ -1,10 +1,11 @@
 """Api module for the project."""
+import json
+from collections import Counter
 from pathlib import Path
 
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-RECIPES_FILE_PATH = (Path("../json_files/parsed_recipes.json"))
 
 
 @app.get("/")
@@ -13,10 +14,22 @@ def read_root() -> dict:
     return {"App": "Recipes API"}
 
 
-def check_if_recipes_exists(recipes_file_path: Path) -> Path:
+def check_if_recipes_exists() -> Path:
     """Check if file exists and return its path, otherwise raise an exception."""
+    recipes_file_path = Path("../json_files/parsed_recipes.json")
     if not Path.exists(recipes_file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return recipes_file_path
 
 
+@app.get("/categories")
+async def display_amount_of_recipes_per_category() -> dict:
+    """Return amount of recipes from each category."""
+    try:
+        with Path.open(check_if_recipes_exists()) as file:
+            recipes = json.load(file)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=500, detail="Error reading JSON file")  # noqa: B904
+    else:
+        categories: list = [recipe_data["category"].lower() for recipe_data in recipes.values()]
+        return Counter(categories)
