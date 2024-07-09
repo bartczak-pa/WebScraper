d1e0a1bee9064f088ffdb9ac060bdc34e9bbcc53
Pawel Bartczak <pawel@bartczak.tech>
2024-07-09 13:54:19 +0100

    Added function checking if path to the file exist.

--- api/api.py
+++ api/api.py
@@ -1,12 +1,24 @@
 """Api module for the project."""
+from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+FILES_PATH = (Path("../json_files/"))
 
 
 @app.get("/")
 def read_root() -> dict:
     """Return default value in API root."""
     return {"App": "Recipes API"}
+
+
+def check_if_file_exists(file_name: str) -> Path:
+    """Check if file exists and return its path, otherwise raise an exception."""
+    file_path = FILES_PATH / file_name
+
+    if not Path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    return file_path
 
