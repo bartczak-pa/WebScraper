0614adc5590f1822bf20b7b8c63e4f7217f0d5e2
Pawel Bartczak <pawel@bartczak.tech>
2024-07-13 16:37:08 +0100

    Added docker-compose support
    API and scraper has separate Dockerfiles and works in separate containers

--- api/api.py
+++ api/api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 def check_if_recipes_exists() -> Path:
     """Check if file exists and return its path, otherwise raise an exception."""
-    recipes_file_path = Path("../json_files/parsed_recipes.json")
+    recipes_file_path = Path("/app/json_files/parsed_recipes.json")
     if not Path.exists(recipes_file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return recipes_file_path
