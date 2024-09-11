1d81b677f571d6b7bc4f6bfa35be35fd536c3084
Paweł Bartczak <113122068+bartczak-pa@users.noreply.github.com>
2024-09-11 12:57:39 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,4 +5,5 @@ from pathlib import Path
 
 BASE_PATH = Path(os.environ.get("BASE_PATH", "/app")).resolve()
 if not BASE_PATH.is_absolute():
-    raise ValueError("BASE_PATH must be an absolute path")
+    msg = "BASE_PATH must be an absolute path"
+    raise ValueError(msg)
